280e12668c259bb251bcfffc26ceb84cfeca2371
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2017-12-18 15:02:42 -0500

    add dependencies folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,25 @@ var minifyCSS = require('gulp-csso');
 var plumber = require('gulp-plumber');
 var path = require('path');
 
-gulp.task('css', function(){
-  return gulp.src('styles/less/*.less')
-    .pipe(less())
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('styles/css'))
-});
-
-gulp.task('default', ['css']);
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function() {
+  gulp.src(['node_modules/bootstrap/less/**/*'])
+    .pipe(gulp.dest('styles/dependencies/bootstrap'))
+  gulp.src(['node_modules/font-awesome/**/*'])
+    .pipe(gulp.dest('styles/dependencies/font-awesome'))
+  gulp.src(['node_modules/patternfly/dist/less/**/*'])
+    .pipe(gulp.dest('styles/dependencies/patternfly'))
+})
 
 // Compiles less on to /css
 gulp.task('less', function () {
   gulp.src('styles/less/fabric8.less')
     .pipe(plumber())
     .pipe(less({
-        paths: [ path.join('node_modules'), path.join('node_modules/patternfly/dist/less/dependencies'), path.join('node_modules/bootstrap/less'), path.join('node_modules/font-awesome/less')],
-        // sourceMap: {
-        //     sourceMapRootpath: '/' // This one for PF files (Optional absolute or relative path to your LESS files)
-        // }
+        paths: [ path.join('node_modules'), path.join('styles/dependencies/patternfly/'), path.join('styles/dependencies/bootstrap/less'), path.join('styles/dependencies/font-awesome/less')],
     }))
     .pipe(minifyCSS())
     .pipe(gulp.dest('styles/css'))
 });
+
+gulp.task('default', ['copy', 'less']);
